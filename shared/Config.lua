b2a3bfc7c538d18ba86c20a49faea5b2e8f3a770
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2024-06-30 22:34:37 -0400

    Update Config.lua

--- shared/Config.lua
+++ shared/Config.lua
@@ -1,9 +1,9 @@
 Config = {}
 
-Config.NPC = vector4(185.29, -916.61, 30.69, 148.52) -- Location for the NPC to spawn.
+Config.NPC = vector4(-247.96, -911.43, 32.34, 183.31) -- Location for the NPC to spawn.
 Config.Model = "cs_fbisuit_01" -- NPC Model.
-Config.VehicleSpawn = vector4(180.45, -923.15, 30.69, 230.33) -- Location for car to spawn.
-Config.Garage = "garage1" -- Default garage for the car to be tagged too.
+Config.VehicleSpawn = vector4(-208.95, -915.7, 29.2, 152.25) -- Location for car to spawn.
+Config.Garage = "pillbox" -- Default garage for the car to be tagged too.
 
 Config.CoinShop = {
     [1] = {
@@ -16,10 +16,10 @@ Config.CoinShop = {
     },
     [2] = {
         type = "car", -- Item/Car
-        name = "sultan", -- Model of the vehicle to spawn.
-        cost = 1, -- Coin Cost to purchase item.
-        header = "Buy Sultan", -- Header Text.
-        text = "Buy a Sultan for 1 coin.", -- Sub Text.
+        name = "cadct5beast", -- Model of the vehicle to spawn.
+        cost = 1000, -- Coin Cost to purchase item.
+        header = "Buy Cadillac CT Beast", -- Header Text.
+        text = "Purchase to add to your garage.", -- Sub Text.
     },
 }
 
