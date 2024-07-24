b9835f86e8e4cd0712fe3f64708be591e2df5b35
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2024-07-24 19:12:30 -0400

    Update Config.lua

--- shared/Config.lua
+++ shared/Config.lua
@@ -6,15 +6,15 @@ Config.VehicleSpawn = vector4(-208.95, -915.7, 29.2, 152.25) -- Location for car
 Config.Garage = "pillbox" -- Default garage for the car to be tagged too.
 
 Config.CoinShop = {
+    -- [1] = {
+    --     type = "item", -- Item/Car
+    --     name = "weapon_pistol", -- Name of Item.
+    --     amount = 1, -- amount of items to give.
+    --     cost = 1, -- Coin Cost to purchase item.
+    --     header = "Buy Pistol", -- Header Text.
+    --     text = "Buy pistol for 1 coin.", -- Sub Text.
+    -- },
     [1] = {
-        type = "item", -- Item/Car
-        name = "weapon_pistol", -- Name of Item.
-        amount = 1, -- amount of items to give.
-        cost = 1, -- Coin Cost to purchase item.
-        header = "Buy Pistol", -- Header Text.
-        text = "Buy pistol for 1 coin.", -- Sub Text.
-    },
-    [2] = {
         type = "car", -- Item/Car
         name = "cadct5beast", -- Model of the vehicle to spawn.
         cost = 1000, -- Coin Cost to purchase item.
